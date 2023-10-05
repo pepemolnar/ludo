17fb8d95a8835a16d066e46aac724e6530919787
Pepe <peter.molnar.h@gmail.com>
2023-10-05 11:57:43 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import * as _ from "lodash";
+var numbers = [
+    1,
+    5,
+    8,
+    10,
+    1,
+    5,
+    15,
+    42,
+    5
+];
+var uniqNumbers = _.uniq(numbers);
+console.log(uniqNumbers);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as _ from "lodash";
+
+const numbers = [1, 5, 8, 10, 1, 5, 15, 42, 5];
+
+const uniqNumbers = _.uniq(numbers);
+
+console.log(uniqNumbers);
